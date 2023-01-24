6ca99c077945b6a72cecc1e7749cda48f63e97bf
Felippe Rian <felipperian365@gmail.com>
2023-01-24 16:12:43 -0300

    Adicionando movimento e direções para o rato

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from Stack import Stacks
+
 class Maze:
     def __init__(self):
         self.maze = None
@@ -11,9 +12,120 @@ class Maze:
             'row': None,
         }
 
+    def canMoveToDown(self):
+        row = self.mouse_coords['row']
+        column = self.mouse_coords['column']
+        down_position = self.maze[row+1][column]
+        if (down_position != '0'):
+            return False
+        if(down_position is None):
+            return False
+        
+        return True
+    
+    def canMoveToUp(self):
+        row = self.mouse_coords['row']
+        column = self.mouse_coords['column']
+        up_position = self.maze[row-1][column]
+        if (up_position != '0'):
+            return False
+        if(up_position is None):
+            return False
+        
+        return True
+
+    def canMoveToRight(self):
+        row = self.mouse_coords['row']
+        column = self.mouse_coords['column']
+        right_position = self.maze[row+1][column]
+        
+        if (right_position is None):
+            return False
+        if(right_position != '0'):
+            return False
+        
+        return True
+    
+    def canMoveToLeft(self):
+        row = self.mouse_coords['row']
+        column = self.mouse_coords['column']
+        left_position = self.maze[row][column-1]
+        
+        if (left_position is None):
+            return False
+        if(left_position != '0'):
+            return False
+        
+        return True
+
+    def moveToLeft(self):
+        if not self.canMoveToLeft():
+            return False
+        
+        row = self.mouse_coords['row']
+        column = self.mouse_coords['column']
+        self.moveMouse({
+            'column': column-1,
+            'row': row,
+        })
+    
+    def moveToRight(self):
+        if not self.canMoveToRight():
+            return False
+        
+        row = self.mouse_coords['row']
+        column = self.mouse_coords['column']
+        self.moveMouse({
+            'column': column+1,
+            'row': row,
+        })
+    
+    def moveToDown(self):
+        if not self.canMoveToDown():
+            return False
+        
+        row = self.mouse_coords['row']
+        column = self.mouse_coords['column']
+        self.moveMouse({
+            'column': column,
+            'row': row+1,
+        })
+    
+    def moveToUp(self):
+        if not self.canMoveToUp():
+            return False
+
+        row = self.mouse_coords['row']
+        column = self.mouse_coords['column']
+        
+        self.moveMouse({
+            'column': column,
+            'row': row-1,
+        })
+
     def showMaze(self):
         for row_number in range(len(self.maze)):
             print(self.maze[row_number])
+
+    def findMousePosition(self):
+        for row_number in range(len(self.maze)):
+            find_mouse = ''.join(self.maze[row_number]).find('m')
+            if find_mouse >= 0:
+                self.mouse_coords['column'] = find_mouse
+                self.mouse_coords['row'] = row_number
+    
+    def findExitPosition(self):
+        for row_number in range(len(self.maze)):
+            find_exit = ''.join(self.maze[row_number]).find('e')
+            if find_exit >= 0:
+                self.exit_coords['column'] = find_exit
+                self.exit_coords['row'] = row_number
+    
+    def moveMouse(self, position_coords):
+        self.maze[position_coords['row']][position_coords['column']] = 'm'
+        self.maze[self.mouse_coords['row']][self.mouse_coords['column']] = '0'
+        self.mouse_coords = position_coords
+        print(position_coords)
 
     def mountMaze(self):
         maze_data = open('maze.txt', 'r')
@@ -39,11 +151,7 @@ class Maze:
         self.maze.insert(0, wallUpAndDown1)
         self.maze.append(wallUpAndDown2)
 
-        for row_number in range(len(self.maze)):
-            find_exit = ''.join(self.maze[row_number]).find('e')
-            if find_exit >= 0:
-                self.exit_coords['column'] = find_exit
-                self.exit_coords['row'] = row_number
+        self.findExitPosition()
                 
         self.maze[self.exit_coords['row']][self.exit_coords['column']] = '0'
         
@@ -56,11 +164,18 @@ class Maze:
         elif self.exit_coords['column'] == len(self.maze[0])-2:
             self.exit_coords['column'] = self.exit_coords['column']+1
         else:
-            print("Deus sabe")
+            print("(!) Erro inesperado ao montar o labirinto")
         
         self.maze[self.exit_coords['row']][self.exit_coords['column']] = 'e'
+        self.findMousePosition()
+        print()
         self.showMaze()
-        print(self.exit_coords)
+        print()
+        self.moveToLeft()
+        self.moveToLeft()
+        self.moveToUp()
+        self.moveToDown()
+        self.showMaze()
 
 mazze = Maze()
 mazze.mountMaze()
